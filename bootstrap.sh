5dbbf3fce01de84c15c708e1627e181abfc5191f
Administrator <Administrator@INCStest01.local>
2015-04-19 22:12:59 +0530

    Moved Vagrantfile and bootstrap.sh to backend-centos

--- bootstrap.sh
+++
@@ -1,5 +0,0 @@
-echo "Welcome to DevOps 101"
-hostname dev.devops101.com
-yum -y install ruby ruby-lib ruby-shadow puppet
-hostname dev.devops101.com
-echo '192.168.33.10 dev dev.devops101.com' >> /etc/hosts
