7ef5dddb4a6db1bed855c96aa4e05a6cce8d4a97
Sreedevi <suryasreevedula@gmail.com>
2015-04-21 21:24:12 +0530

    Updated hostname of backend-centos

--- backend-centos/bootstrap.sh
+++ backend-centos/bootstrap.sh
@@ -1,5 +1,5 @@
 echo "Welcome to DevOps 101"
 hostname dev.devops101.com
 yum -y install ruby ruby-lib ruby-shadow puppet
-hostname dev.devops101.com
-echo '192.168.33.10 dev dev.devops101.com' >> /etc/hosts
+hostname backend-centos.devops101.com
+echo '192.168.33.10 backend-centos backend-centos.devops101.com' >> /etc/hosts
