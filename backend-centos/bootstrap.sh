91beccbbebe6605e75e630fc8157b53cf3bb771e
Sreedevi <suryasreevedula@gmail.com>
2015-04-23 02:05:13 +0530

    Removed puppet provisioning

--- backend-centos/bootstrap.sh
+++ backend-centos/bootstrap.sh
@@ -1,5 +1,3 @@
 echo "Welcome to DevOps 101"
-hostname dev.devops101.com
-yum -y install ruby ruby-lib ruby-shadow puppet
 hostname backend-centos.devops101.com
 echo '192.168.33.10 backend-centos backend-centos.devops101.com' >> /etc/hosts
